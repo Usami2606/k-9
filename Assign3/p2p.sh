d49e7d27e032969e4e3779fd45206834a9967050
usami2606 <usami2606tsukuba@gmail.com>
2025-05-27 10:19:33 +0900

    Initial working version completed

--- Assign3/p2p.sh
+++ Assign3/p2p.sh
@@ -3,10 +3,10 @@
 # initial value
 value=256
 
-# 繰り返し回数
+# Repeat Count
 count=12
 
-# ループして ./a.out を実行
+# Loop and run the executable file
 for ((i=1; i<=count; i++)); do
     mpirun ./bin/runprog "$value"
     value=$((value * 2))
